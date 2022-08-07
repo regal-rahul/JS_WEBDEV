d3dbd22223b62e5eaa31eb1577608dbb994c31b9
regal-rahul <regalrahul777@gmail.com>
2022-08-07 14:58:26 +0530

    temperature convertion

--- 01_Basic/celcius.js
+++ 01_Basic/celcius.js
@@ -1,20 +1,35 @@
-var score = 100;
-score = 130;
-const bonus = 50;
+// var score = 100;
+// score = 130;
+// const bonus = 50;
 
 // var finalScore = score + bonus;
 // var finalScore = (score + bonus) * 1.8;
 
 // var complex = 4 + 4 * 5 / 2; // bad practice
-var complex = ((4 + 4) * 5) / 2; // good practice, this expression is only for demonstration
+// var complex = ((4 + 4) * 5) / 2; // good practice, this expression is only for demonstration
 
 // console.log(finalScore);
 // console.log(complex);
 
 // % modulo operator, perfroms division but gives only remainder as output
 
-var num1 = 17;
-var num2 = 9;
+// var num1 = 17;
+// var num2 = 9;
 
-var mod = num1 % num2;
-console.log(mod);
+// var mod = num1 % num2;
+// console.log(mod);
+
+// TODO converting temperature from fahrenheri to celcius
+
+let tempFahrenheit = 105;
+// calculation
+
+let celcius = (tempFahrenheit -32) * (5 / 9);
+console.log(celcius);
+
+// TODO converting temperature from celcius to fahrenheit
+
+let tempCelcius = 41;
+//calculation
+let fahrenheit = (tempCelcius * (9 / 5)) + 32;
+console.log(fahrenheit);
