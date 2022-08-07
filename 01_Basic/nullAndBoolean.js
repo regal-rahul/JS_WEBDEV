6a85f90744130343c6d862be780ed8afa8049c51
regal-rahul <regalrahul777@gmail.com>
2022-08-07 15:14:03 +0530

    operators

--- 01_Basic/nullAndBoolean.js
+++ 01_Basic/nullAndBoolean.js
@@ -1,5 +1,22 @@
 // var temperature = 0;
 var temperature;
 
-console.log("Current temperature is: " + temperature);
+// console.log("Current temperature is: " + temperature);
 
+//Grade system
+
+// 10 - Great job
+// 5 - Work Harder
+// 2 - Poor
+// 0 - Fail
+// Boolean - true'false
+
+// let's do a grade comparison
+
+// let grade = true;
+// console.log(grade);
+
+let actualMarks = 9;
+let myGrade = (actualMarks == 10)
+
+console.log(myGrade);
