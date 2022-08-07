a80af0c30cb25fe96db2c5f64e19569b27e594a4
regal-rahul <regalrahul777@gmail.com>
2022-08-07 15:26:25 +0530

    ifelseif

---
+++ 02_Basic/ifElse.js
@@ -0,0 +1,15 @@
+// if (false) {
+//     console.log("True");
+// } else {
+//     console.log("False");
+// }
+
+var whoIsHere = 'spam';
+
+if (whoIsHere === 'admin') {
+    console.log("Access to view content");
+} else if (whoIsHere === 'recruiter') {
+    console.log("Access to contacts and works.");
+} else {
+    console.log("Choose a role");
+}
