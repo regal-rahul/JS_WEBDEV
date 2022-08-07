bdfa5305a6e9c63c9ac2d21885e3687ed602db2f
regal-rahul <regalrahul777@gmail.com>
2022-08-07 14:36:47 +0530

    let variable

---
+++ 01_Basic/variables.js
@@ -0,0 +1,18 @@
+let name = 'Hulk';
+
+// let score = '108';
+// let bonus = '20';
+// let totalScore = score + bonus;
+// console.log(totalScore);
+
+let score = 108;
+let bonus = 20;
+// let totalScore = score + bonus;
+// console.log(totalScore);
+// console.log(name);
+
+let firstName = 'Hulk';
+let lastName = 'Banner';
+// let fullName = firstName + " " + lastName;
+// console.log(fullName);
+console.log(firstName + " " +lastName);
