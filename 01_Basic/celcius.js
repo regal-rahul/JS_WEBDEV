d60fdcf8336aaea72f8f029324b35751818efb32
regal-rahul <regalrahul777@gmail.com>
2022-08-07 14:50:41 +0530

    moduko operator

---
+++ 01_Basic/celcius.js
@@ -0,0 +1,20 @@
+var score = 100;
+score = 130;
+const bonus = 50;
+
+// var finalScore = score + bonus;
+// var finalScore = (score + bonus) * 1.8;
+
+// var complex = 4 + 4 * 5 / 2; // bad practice
+var complex = ((4 + 4) * 5) / 2; // good practice, this expression is only for demonstration
+
+// console.log(finalScore);
+// console.log(complex);
+
+// % modulo operator, perfroms division but gives only remainder as output
+
+var num1 = 17;
+var num2 = 9;
+
+var mod = num1 % num2;
+console.log(mod);
