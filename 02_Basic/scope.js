45632d38e51c80b7d07fce9d29c7f865d35594e4
regal-rahul <regalrahul777@gmail.com>
2022-08-08 13:15:39 +0530

    hoisting and local global declaration

--- 02_Basic/scope.js
+++ 02_Basic/scope.js
@@ -1,12 +1,30 @@
-let iAmGlobal = 'Tony Stark';
+// let iAmGlobal = 'Tony Stark';
+
+// if (true) {
+//     // let iAmLocal = 'HULK';
+//     var iAmLocal = 'HULK';
+//     iAmGlobal = 'Superman';
+//     console.log(iAmGlobal);
+//     console.log(iAmLocal);
+// }
+
+// console.log(iAmLocal);
+// console.log(iAmGlobal);
+
+// TODO Kings Problem
+
+// let king = 'John';
 
 if (true) {
-    // let iAmLocal = 'HULK';
-    var iAmLocal = 'HULK';
-    iAmGlobal = 'Superman';
-    console.log(iAmGlobal);
-    console.log(iAmLocal);
+    // let king = 'Sam';
+
+    if (true) {
+        // let king = 'Ram';
+        let king = 'Ram';
+        console.log(king);
+    }
 }
 
-console.log(iAmLocal);
-console.log(iAmGlobal);
+if (true) {
+    console.log("I am second part " + king);
+}
