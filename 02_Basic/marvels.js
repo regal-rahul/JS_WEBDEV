e842900a088ea2ca02b899abc1d05fc18acb37c0
regal-rahul <regalrahul777@gmail.com>
2022-08-11 18:07:24 +0530

    tilde

---
+++ 02_Basic/marvels.js
@@ -0,0 +1,9 @@
+// datatype - Array
+
+const superHeroes = ['Iron Man', 'Spiderman', 'Hulk', 'Capt. America'];
+
+console.log(superHeroes);
+console.log(superHeroes[0]);
+console.log(superHeroes[superHeroes.length - 1]);
+
+console.log(`Total superheroes are ${superHeroes.length - 1}`);
