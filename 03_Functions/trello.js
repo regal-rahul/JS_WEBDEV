323b474f8a3234466a2368cbb6f6478bc34986f3
regal-rahul <regalrahul777@gmail.com>
2022-08-13 16:35:37 +0530

    looping through array

---
+++ 03_Functions/trello.js
@@ -0,0 +1,24 @@
+const days = ['Mon', 'Tue', 'Wed', 'Thr', 'Fri', 'Sat', 'Sun'];
+
+// console.log(days[0]);
+
+// let sayHello = function (name) {
+    // console.log(`Greeting message for ${name}`);
+    // console.log(`Hey ${name}`);
+// };
+
+// days.forEach(function (day, index) {
+//     console.log(`Today is ${day} at ${index + 1}`);
+// });
+
+const months = ['Jan','Feb','Mar','Apr','May','June','July','Aug','Sept','Oct','Nov','Dec'];
+
+// months.forEach((month, index) => {
+//     console.log(`Month ${index + 1} is ${month}`);
+// });
+
+const todos = ['Clean House', 'Buy a shirt', 'Learn JS', 'Build a project'];
+
+todos.forEach((todo, index) => {
+    console.log(`On day ${index + 1} ${todo}`);
+});
