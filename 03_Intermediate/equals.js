f3c299ed0c9555be049924e1b45e0600ed55ea25
regal-rahul <regalrahul777@gmail.com>
2022-08-13 18:10:19 +0530

    objects

---
+++ 03_Intermediate/equals.js
@@ -0,0 +1,4 @@
+let a = {};
+let b = a;
+
+console.log({} === {});
---
+++ 04_Intermediate_2/myObjects.js
@@ -0,0 +1,9 @@
+let myProjects = {
+    title: 'Go Camp',
+    author: 'Rahul',
+    appType: 'Web App',
+    description: 'It is a web app to find and publish campgrounds.'
+}
+
+console.log(myProjects);
+console.log(`Hey the project ${myProjects.title} made by ${myProjects.author} is a ${myProjects.appType} and ${myProjects.description}`);
