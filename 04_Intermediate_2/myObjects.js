2b97c0ddf6b131a5449a196ec77ffa666f85f3f7
regal-rahul <regalrahul777@gmail.com>
2022-08-15 11:42:40 +0530

    objects

--- 04_Intermediate_2/myObjects.js
+++ 04_Intermediate_2/myObjects.js
@@ -5,5 +5,27 @@ let myProjects = {
     description: 'It is a web app to find and publish campgrounds.'
 }
 
-console.log(myProjects);
-console.log(`Hey the project ${myProjects.title} made by ${myProjects.author} is a ${myProjects.appType} and ${myProjects.description}`);
+// console.log(myProjects);
+// console.log(`Hey the project ${myProjects.title} made by ${myProjects.author} is a ${myProjects.appType} and ${myProjects.description}`);
+
+// myProjects.title = 'Quote World';
+// console.log(`Hey the project ${myProjects.title} made by ${myProjects.author} is a ${myProjects.appType} and ${myProjects.description}`);
+// console.log(myProjects);
+
+let myProjectsTwo = {
+    title: 'Quote World',
+    author: 'Regal',
+    appType: 'Web App',
+    description: 'It is a web app for random quote'
+}
+
+let changeAuthor = function (myObject) {
+    return {
+        formatOne: `Author of one format is: ${myObject.author}`,
+        formatTwo: `Author of two format is: ${myObject.author} two`
+    };
+
+}
+
+let format = changeAuthor(myProjects);
+console.log(format.formatTwo);
