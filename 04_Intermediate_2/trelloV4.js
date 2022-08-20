894142b6cfd8aeeafbad35cb699b24432a135912
regal-rahul <regalrahul777@gmail.com>
2022-08-20 14:14:16 +0530

    search

---
+++ 04_Intermediate_2/trelloV4.js
@@ -0,0 +1,49 @@
+// const myTodos = ['Buy Bread', 'Go to Gym', 'Practice Code'];
+
+// console.log(myTodos[myTodos.indexOf('Buy Bread')]);
+
+
+const newTodos = [
+    {
+        title: 'Buy Bread',
+        idDone: false
+    },
+    {
+        title: 'Go to Gym',
+        idDone: false
+    },
+    {
+        title: 'Practice Code',
+        idDone: true
+    }
+];
+
+// const index = newTodos.findIndex(function (todo, index) {
+//     console.log(todo);
+//     return todo.title === "Go to Gym";
+// });
+
+// console.log(index);
+
+
+// 1 Method
+// const findTodo = function (myTodos, title) {
+//     const index = myTodos.findIndex(function (todo) {
+//         return todo.title.toLowerCase() === title.toLowerCase();
+//     })
+//     return myTodos[index];
+// }
+
+// let result = findTodo(newTodos, 'go To gym');
+// console.log(result);
+
+// 2 method
+
+const findTodo = function (myTodos, title) {
+    const titleReturned = myTodos.find(function (todo) {
+        return todo.title.toLowerCase() === title.toLowerCase()
+    })
+    return titleReturned;
+}
+let result = findTodo(newTodos, 'go To gym');
+console.log(result);
