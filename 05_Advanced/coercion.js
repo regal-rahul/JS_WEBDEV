2b225ed53cecbb7e06f49c5db2d26a36a9282ac0
regal-rahul <regalrahul777@gmail.com>
2022-08-22 10:02:13 +0530

    coercion

---
+++ 05_Advanced/coercion.js
@@ -0,0 +1,30 @@
+// console.log("5" - 5);
+
+// const giveType = typeof 5;
+
+// console.log(giveType);
+
+
+const adder = 5 + false;
+
+// console.log(adder);
+
+const loginDetails = [
+        // Logic for getting details for db.
+];
+
+const loginID = loginDetails[0];
+if (loginID) {
+    console.log('Allow user to login');
+} else {
+    console.log('Authentication failed!');
+}
+
+// Values that interpret as false:
+// false 
+// 0
+// ""
+// null
+// undefined
+
+// And everything else is true
