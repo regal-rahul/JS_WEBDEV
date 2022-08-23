35d6f4151759b21eb407b67446ece088bf03cfb8
regal-rahul <regalrahul777@gmail.com>
2022-08-23 11:31:07 +0530

    event listener

--- 06_WebBasics/app.js
+++ 06_WebBasics/app.js
@@ -15,3 +15,12 @@ myParaElement.forEach((p) => (p.textContent = 'I am being changed using loop in 
 const myNewPara = document.createElement('p');
 myNewPara.textContent = 'I was added using JS';
 document.querySelector('body').appendChild(myNewPara);
+
+const myButton = document.querySelector('button');
+
+myButton.addEventListener('click', (e) => {
+    // alert('Button clicked');
+    // console.log('Button clicked');
+    console.log(e);
+    e.target.textContent = 'Clicked';
+})
